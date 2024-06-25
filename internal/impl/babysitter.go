1dd2b47b6c3fe2a60236741a1bdd5a01a1b5317f
Rodrigo Boratto <rodrigo.rwrz@gmail.com>
2024-06-25 01:52:03 -0300

    removing retry logic, replacing with panic

--- internal/impl/babysitter.go
+++ internal/impl/babysitter.go
@@ -153,20 +153,13 @@ func (b *babysitter) Serve() error {
 // ActivateComponent implements the envelope.EnvelopeHandler interface.
 func (b *babysitter) ActivateComponent(ctx context.Context, request *protos.ActivateComponentRequest) (*protos.ActivateComponentReply, error) {
 	go func() {
-		for {
-			if err := b.watchPods(b.ctx, request.Component); err != nil {
-				// TODO(mwhittaker): Log this error.
-				fmt.Fprintf(os.Stderr, "watchPods(%q): %v", request.Component, err)
-				b.logger.Error("error watching pods", "err", err, "component", request.Component)
-			} else {
-				return
-			}
+		if err := b.watchPods(b.ctx, request.Component); err != nil {
+			// TODO(mwhittaker): Log this error.
+			fmt.Fprintf(os.Stderr, "watchPods(%q): %v", request.Component, err)
+			b.logger.Error("error watching pods", "err", err, "component", request.Component)
 
-			select {
-			case <-ctx.Done():
-				return
-			case <-time.After(5 * time.Second): // wait 5 second before retrying
-			}
+			// errors are critical
+			panic(err)
 		}
 	}()
 	return &protos.ActivateComponentReply{}, nil
